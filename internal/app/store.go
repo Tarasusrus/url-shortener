3673a769ac3358adc6736288a744f965a3e717ea
tarasusrus <bar.norilsk@gmail.com>
2024-01-31 20:32:26 +0300

    Implement new Store structure in store.go
    Added a new structure, Store, in store.go to handle URL shortening. The structure includes a map to store short and full URLs, and functions such as Set and Get to add and retrieve data from this map. Concurrent access to data is safely managed using the sync package's RWMutex.

---
+++ internal/app/store.go
@@ -0,0 +1,56 @@
+package app
+
+// Импортируем нужные нам пакеты
+import (
+	"math/rand"
+	"sync"
+	"time"
+)
+
+// Определяем константу символов для генерации короткого ID
+const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+// Store Структура Store с Read/Write Mutex и map для хранения коротких и полных URL
+type Store struct {
+	sync.RWMutex
+	urls map[string]string
+}
+
+// NewStore Функция для создания нового Store с пустой map
+func NewStore() *Store {
+	return &Store{
+		urls: make(map[string]string),
+	}
+}
+
+// Set Функция для добавления нового URL в Store. Мы генерируем короткий ID, добавляем его в map,
+// а затем возвращаем этот ID. Блокировка обеспечивает, что добавление происходит атомарно.
+func (s *Store) Set(url string) string {
+	s.Lock()
+	defer s.Unlock()
+	id := generateID()
+	s.urls[id] = url
+	return id
+}
+
+// Get Функция для получения полного URL по короткому идентификатору из Store.
+// Если идентификатор найден, возвращается полный URL и флаг exists будет true.
+// Если идентификатор не найден, возвращается пустая строка и флаг exists будет false.
+// Read Lock используется для обеспечения безопасного доступа на чтение.
+func (s *Store) Get(id string) (string, bool) {
+	s.RLock()
+	defer s.RUnlock()
+	url, exists := s.urls[id]
+	return url, exists
+}
+
+// Функция для генерации случайного идентификатора из заданного набора символов (charset).
+// Идентификатор генерируется как строка длиной 10 символов.
+func generateID() string {
+	rand.Seed(time.Now().UnixNano())
+	b := make([]byte, 10)
+	for i := range b {
+		b[i] = charset[rand.Intn(len(charset))]
+	}
+	return string(b)
+}
