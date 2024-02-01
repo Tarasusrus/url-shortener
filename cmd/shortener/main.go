f1a65f5fc94336a26ade65abb3d54c738ede11f4
tarasusrus <bar.norilsk@gmail.com>
2024-02-01 13:53:05 +0300

    Split HTTP handling into separate methods in shortener command
    HTTP handling in the shortener command has been separated into two different methods, `handlePost` and `handleGet`. These methods are responsible for handling POST and GET requests respectively. This helps improve the readability and maintainability of the code.

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -8,57 +8,56 @@ import (
 	"strings"
 )
 
+// handlePost обрабатывает POST-запросы
+func handlePost(w http.ResponseWriter, r *http.Request, store *app.Store) {
+	// Проверяем, что ContentType запроса text/plain
+	// Если это не так, то возвращает код ошибки 400
+	mediaType, _, err := mime.ParseMediaType(r.Header.Get("Content-Type"))
+	if err != nil || mediaType != "text/plain" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	body, err := io.ReadAll(r.Body)
+	defer r.Body.Close()
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	// Преобразуем тело запроса в строку, сокращаем URL и сохраняем его
+	url := string(body)
+	id := store.Set(url)
+
+	// Отправляем ответ с кодом 201 и сокращенным URL
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte(id))
+}
+
+// handleGet обрабатывает GET-запросы
+func handleGet(w http.ResponseWriter, r *http.Request, store *app.Store) {
+	id := strings.TrimPrefix(r.URL.Path, "/")
+	url, ok := store.Get(id)
+	if ok {
+		w.Header().Set("Location", url)
+		w.WriteHeader(http.StatusTemporaryRedirect)
+		return
+	}
+	w.WriteHeader(http.StatusBadRequest)
+}
+
 func main() {
 	store := app.NewStore()
-
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		switch r.Method {
 		case http.MethodPost:
-			// Проверяем, что ContentType запроса text/plain
-			// Если это не так, то возвращает код ошибки 400
-			mediaType, _, err := mime.ParseMediaType(r.Header.Get("Content-Type"))
-			if err != nil {
-				// Возвращаем ошибку, если не можем парсить.MediaType
-				w.WriteHeader(http.StatusBadRequest)
-				return
-			}
-			if mediaType != "text/plain" {
-				// Возвращаем ошибку, если MediaType не "text/plain"
-				w.WriteHeader(http.StatusBadRequest)
-				return
-			}
-
-			// Читаем тело запроса
-			body, err := io.ReadAll(r.Body)
-			defer r.Body.Close()
-			if err != nil {
-				w.WriteHeader(http.StatusBadRequest)
-			}
-
-			// Преобразуем тело запроса в строку, сокращаем URL и сохраняем его
-			url := string(body)
-			id := store.Set(url)
-
-			// Отправляем ответ с кодом 201 и сокращенным URL
-			w.WriteHeader(http.StatusCreated)
-			w.Write([]byte(id))
-
+			handlePost(w, r, store)
 		case http.MethodGet:
-			id := strings.TrimPrefix(r.URL.Path, "/")
-			url, ok := store.Get(id)
-			if ok {
-				w.Header().Set("Location", url)
-				w.WriteHeader(http.StatusTemporaryRedirect)
-				return
-			}
-			w.WriteHeader(http.StatusBadRequest)
-			return
-
+			handleGet(w, r, store)
 		default:
 			// На любой некорректный запрос сервер должен возвращать ответ с кодом 400
 			w.WriteHeader(http.StatusBadRequest)
 		}
 	})
-
 	http.ListenAndServe(":8080", nil)
 }
