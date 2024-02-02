8f83fee723779e5cbaa3283da2be4da33d71e5b1
tarasusrus <bar.norilsk@gmail.com>
2024-02-02 16:04:12 +0300

    Add POST request handling to URL shortener
    Added a function to handle POST requests in the URL shortener application. This function parses the media type of incoming requests, reads the body, and creates a short URL if the body is not empty. The changes improve the modularization of the code, making maintenance easier.

---
+++ internal/app/handlers/post.go
@@ -0,0 +1,45 @@
+package handlers
+
+import (
+	"github.com/Tarasusrus/url-shortener/internal/app/stores"
+	"io"
+	"log"
+	"mime"
+	"net/http"
+)
+
+// HandlePost обрабатывает POST-запросы
+func HandlePost(w http.ResponseWriter, r *http.Request, store *stores.Store) {
+	// Проверяем, что ContentType запроса text/plain
+	// Если это не так, то возвращает код ошибки 400
+	mediaType, _, err := mime.ParseMediaType(r.Header.Get("Content-Type"))
+	if err != nil || mediaType != "text/plain" {
+		log.Printf("Error parsing media type or media type is not text/plain: %v\n", err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	body, err := io.ReadAll(r.Body)
+	defer r.Body.Close()
+	if err != nil {
+		log.Printf("Error reading request body: %v\n", err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	log.Println(len(body))
+	if len(body) == 0 {
+		log.Printf("Received empty request body\n")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	// Преобразуем тело запроса в строку, сокращаем URL и сохраняем его
+	url := string(body)
+	id := store.Set(url)
+	log.Printf("Short URL created: %s\n", id)
+
+	// Отправляем ответ с кодом 201 и сокращенным URL
+	w.WriteHeader(http.StatusCreated)
+	urlPath := "http://localhost:8080/" + id
+	w.Write([]byte(urlPath))
+}
