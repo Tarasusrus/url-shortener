5274dda9eefa388788a65e66114e7c349ccb5048
tarasusrus <bar.norilsk@gmail.com>
2024-02-02 19:52:07 +0300

    Update unit tests in post handler
    Enhanced the 'TestHandlePost' function by adding two new test cases to verify the handler's behavior for different scenarios, particularly around content type checking and URL validation. The updated tests now check the response status code, ensuring that the HTTP Post request handler behaves as expected.

--- internal/app/handlers/post_test.go
+++ internal/app/handlers/post_test.go
@@ -1,7 +1,74 @@
 package handlers
 
-import "testing"
+import (
+	"github.com/Tarasusrus/url-shortener/internal/app/stores"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
 
 func TestHandlePost(t *testing.T) {
-	t.Errorf("TestHandlePost failed")
+	type args struct {
+		w     *httptest.ResponseRecorder
+		r     *http.Request
+		store *stores.Store
+	}
+
+	type want struct {
+		body        string
+		contentType string
+		ID          string
+		statusCode  int
+	}
+
+	tests := []struct {
+		name string
+		want want
+		args args
+	}{
+		{
+			name: "Test case 1 - Check Content - Type",
+			args: args{
+				w:     httptest.NewRecorder(),
+				r:     httptest.NewRequest("POST", "/ajdbkl", nil),
+				store: stores.NewStore(),
+			},
+			want: want{
+				body:       "",
+				statusCode: 400,
+			},
+		},
+
+		{
+			name: "Test case 2 - Check URL",
+			args: args{
+				w: httptest.NewRecorder(),
+				r: httptest.NewRequest(
+					"POST",
+					"/tests-id",
+					strings.NewReader("http://localhost:8080/"),
+				),
+				store: &stores.Store{
+
+					Urls: map[string]string{"tests-id": "http://localhost:8080/"},
+				},
+			},
+			want: want{
+				body:       "http://localhost:8080/tests-id",
+				statusCode: 201,
+			},
+		},
+	}
+	for _, tt := range tests {
+
+		req := tt.args.r
+		req.Header.Set("Content-Type", "text/plain")
+
+		t.Run(tt.name, func(t *testing.T) {
+			HandlePost(tt.args.w, tt.args.r, tt.args.store)
+			assert.Equal(t, tt.want.statusCode, tt.args.w.Code)
+		})
+	}
 }
