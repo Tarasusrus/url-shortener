6bfb7b97c7ec4e7a3397f63c08e3d634da30ae23
tarasusrus <bar.norilsk@gmail.com>
2024-02-02 16:33:21 +0300

    Add basic test skeletons for handlers and stores
    Test functions have been added across multiple files including get_test.go, post_test.go, server_test.go, and store_test.go. These simply contain an error indicating failure, serving as placeholders to be fleshed out in the future.

---
+++ internal/app/handlers/post_test.go
@@ -0,0 +1,7 @@
+package handlers
+
+import "testing"
+
+func TestHandlePost(t *testing.T) {
+	t.Errorf("TestHandlePost failed")
+}
---
+++ internal/app/stores/store_test.go
@@ -0,0 +1,19 @@
+package stores
+
+import "testing"
+
+func TestNewStore(t *testing.T) {
+	t.Errorf("TestRun failed")
+}
+
+func TestStore_Set(t *testing.T) {
+	t.Errorf("TestRun failed")
+}
+
+func TestStore_Get(t *testing.T) {
+	t.Errorf("TestRun failed")
+}
+
+func TestGenerateID(t *testing.T) {
+	t.Errorf("TestRun failed")
+}
