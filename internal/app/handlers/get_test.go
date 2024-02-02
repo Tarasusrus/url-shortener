d681ae6faa292d7c747217c410d69ecca2e7a25b
tarasusrus <bar.norilsk@gmail.com>
2024-02-02 18:22:42 +0300

    Expand unit tests in get_test.go and update go.mod dependencies
    Implemented expanded unit tests in `get_test.go` file for the "HandleGet" function and introduced new dependencies in `go.mod`. The new tests have been structured with stubs like arguments and desired responses to provide a clear testing pattern. The added modules in `go.mod` are associated with the testing process.

--- internal/app/handlers/get_test.go
+++ internal/app/handlers/get_test.go
@@ -1,7 +1,49 @@
 package handlers
 
-import "testing"
+import (
+	"github.com/Tarasusrus/url-shortener/internal/app/stores"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
 
-func TestName(t *testing.T) {
-	t.Errorf("Test failed")
+func TestHandleGet(t *testing.T) {
+	type args struct {
+		w     *httptest.ResponseRecorder
+		r     *http.Request
+		store *stores.Store
+	}
+
+	type want struct {
+		body        string
+		contentType string
+		ID          string
+		statusCode  int
+	}
+
+	tests := []struct {
+		name string
+		want want
+		args args
+	}{
+		{
+			name: "Test case 1",
+			args: args{
+				w:     httptest.NewRecorder(),
+				r:     httptest.NewRequest("GET", "/", nil),
+				store: stores.NewStore(),
+			},
+			want: want{
+				body:       "",
+				statusCode: 400,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			HandleGet(tt.args.w, tt.args.r, tt.args.store)
+			assert.Equal(t, tt.want.statusCode, tt.args.w.Code)
+		})
+	}
 }
