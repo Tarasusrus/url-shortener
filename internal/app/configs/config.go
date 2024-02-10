4203bd665a219288160b07edc4762cdf80ff0199
tarasusrus <bar.norilsk@gmail.com>
2024-02-10 11:52:43 +0300

    Refactor flag variable names and access methods
    The names of the flag variables in the `FlagConfig` struct have been capitalized for better visibility. Also, the corresponding access methods have been adjusted to `GetAddress()` and `GetBaseURL()`. On a side note, these changes affect the `config_test.go`, `server.go`, and `post.go` files, where these methods are invoked.

--- internal/app/configs/config.go
+++ internal/app/configs/config.go
@@ -7,17 +7,17 @@ import (
 )
 
 type FlagConfig struct {
-	address string `env:"SERVER_ADDRESS"`
-	baseURL string `env:"BASE_URL"`
+	Address string `env:"SERVER_ADDRESS"`
+	BaseURL string `env:"BASE_URL"`
 }
 
 func NewFlagConfig() *FlagConfig {
 	c := new(FlagConfig)
-	flag.StringVar(&c.address,
+	flag.StringVar(&c.Address,
 		"a",
 		"localhost:8080",
 		"Флаг -a отвечает за адрес запуска HTTP-сервера (значение может быть таким: localhost:8888)")
-	flag.StringVar(&c.baseURL,
+	flag.StringVar(&c.BaseURL,
 		"b",
 		"http://localhost:8080/",
 		"Флаг -b отвечает за базовый адрес результирующего сокращённого URL "+
@@ -32,10 +32,10 @@ func NewFlagConfig() *FlagConfig {
 	return c
 }
 
-func (c *FlagConfig) Address() string {
-	return c.address
+func (c *FlagConfig) GetAddress() string {
+	return c.Address
 }
 
-func (c *FlagConfig) BaseURL() string {
-	return c.baseURL
+func (c *FlagConfig) GetBaseURL() string {
+	return c.BaseURL
 }
