b4cd2cbf54fe301c16d8b287ffe8c42a33ebe4b4
tarasusrus <bar.norilsk@gmail.com>
2024-02-13 10:20:21 +0300

    Update default values and error handling in FlagConfig
    The 'log' import has been removed and error handling updated in the 'NewFlagConfig()' function, now it returns the error instead of calling 'log.Fatal(err)'. Constant default values and descriptions for 'Address' and 'BaseURL' flags have been introduced to improve readability and ease of modification. Changes might require adjustments in related files.

--- internal/app/configs/config.go
+++ internal/app/configs/config.go
@@ -3,7 +3,6 @@ package configs
 import (
 	"flag"
 	"github.com/caarlos0/env"
-	"log"
 )
 
 type FlagConfig struct {
@@ -11,25 +10,23 @@ type FlagConfig struct {
 	BaseURL string `env:"BASE_URL"`
 }
 
-func NewFlagConfig() *FlagConfig {
+const (
+	DefaultAddress         = "localhost:8080"
+	DefaultBaseURL         = "http://" + DefaultAddress + "/"
+	AddressFlagDescription = "Флаг -a отвечает за адрес запуска HTTP-сервера (значение может быть таким: localhost:8888)"
+	BaseURLFlagDescription = "Флаг -b отвечает за базовый адрес результирующего сокращённого URL (значение: адрес сервера перед коротким URL, например http://localhost:8000/)"
+)
+
+func NewFlagConfig() (*FlagConfig, error) {
 	c := new(FlagConfig)
-	flag.StringVar(&c.Address,
-		"a",
-		"localhost:8080",
-		"Флаг -a отвечает за адрес запуска HTTP-сервера (значение может быть таким: localhost:8888)")
-	flag.StringVar(&c.BaseURL,
-		"b",
-		"http://localhost:8080/",
-		"Флаг -b отвечает за базовый адрес результирующего сокращённого URL "+
-			"(значение: адрес сервера перед коротким URL, например http://localhost:8000/)")
+	flag.StringVar(&c.Address, "a", DefaultAddress, AddressFlagDescription)
+	flag.StringVar(&c.BaseURL, "b", DefaultBaseURL, BaseURLFlagDescription)
 	flag.Parse()
-	// Если указана переменная окружения, она будет переопределить значение флага
-	err := env.Parse(c)
-	if err != nil {
-		// обработка ошибок при разборе переменных окружения
-		log.Fatal(err)
+
+	if err := env.Parse(c); err != nil {
+		return nil, err
 	}
-	return c
+	return c, nil
 }
 
 func (c *FlagConfig) GetAddress() string {
