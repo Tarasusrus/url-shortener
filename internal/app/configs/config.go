ecb1aeb39ca6368f16a1ce5a27e9d86ac08fc133
tarasusrus <bar.norilsk@gmail.com>
2024-02-13 10:44:12 +0300

    Add documentation in config.go
    Introduced comments in 'config.go' to explain the FlagConfig structure and server configuration details. This change enhances code readability and understanding, making it easier for other developers to understand the purpose and usage of the server configuration section.

--- internal/app/configs/config.go
+++ internal/app/configs/config.go
@@ -5,11 +5,13 @@ import (
 	"github.com/caarlos0/env"
 )
 
+// FlagConfig Структура содержит данные для настройки сервера
 type FlagConfig struct {
 	Address string `env:"SERVER_ADDRESS"`
 	BaseURL string `env:"BASE_URL"`
 }
 
+// Начальная (по умолчанию) конфигурация сервера
 const (
 	DefaultAddress         = "localhost:8080"
 	DefaultBaseURL         = "http://" + DefaultAddress + "/"
@@ -17,6 +19,7 @@ const (
 	BaseURLFlagDescription = "Флаг -b отвечает за базовый адрес результирующего сокращённого URL (значение: адрес сервера перед коротким URL, например http://localhost:8000/)"
 )
 
+// NewFlagConfig создает новый объект FlagConfig, основываясь на данных из флагов командной строки и переменных окружения.
 func NewFlagConfig() (*FlagConfig, error) {
 	c := new(FlagConfig)
 	flag.StringVar(&c.Address, "a", DefaultAddress, AddressFlagDescription)
